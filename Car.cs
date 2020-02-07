e68de8da5bfb26fbc978d054390bc6bb92c076ed
Dan Cordner <takamine16@gmail.com>
2020-02-07 11:48:24 -0500

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inclass3___cordner
+{
+    class Car
+    {
+        public string Color = "red";
+        int MaxSpeed = 200;
+
+        public void FullThrottle()
+        {
+            Console.WriteLine("I'm given 'er all she's got Captain!!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace inclass3___cordner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Car mycar1 = new Car();
+            Car mycar2 = new Car();
+            Console.WriteLine(mycar1.Color);
+            Console.WriteLine(mycar2.Color);
+            Console.WriteLine(mycar1.FullThrottle);
+
+        }
+    }
+
+ 
+}
